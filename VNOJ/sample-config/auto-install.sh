c39d33e3340a5bf89f1cdb1ef21a092fa654a229
mrkhoa <89059360+khoavnvn@users.noreply.github.com>
2025-09-01 20:28:11 +0700

    Update auto-install.sh

--- VNOJ/sample-config/auto-install.sh
+++ VNOJ/sample-config/auto-install.sh
@@ -10,7 +10,7 @@ sudo apt update -y
 sudo apt install -y mariadb-server libmysqlclient-dev
 sudo service mysql start
 sudo mysql -e "CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;"
-sudo mysql -e "GRANT ALL PRIVILEGES ON dmoj.* TO 'dmoj'@'localhost' IDENTIFIED BY 'greenhat1998';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON dmoj.* TO 'dmoj'@'localhost' IDENTIFIED BY 'Khoa@1989***';"
 mariadb-tzinfo-to-sql /usr/share/zoneinfo | sudo mariadb -u root mysql
 
 # Install with virtual environment
